dff2d6f3f80960965adfd76d87861650c9f314e4
taksrules <Takura.mukaro@outlook.com>
2024-01-22 10:33:21 +0200

    first message

---
+++ skoloapp/business/functions.py
@@ -0,0 +1,19 @@
+from django.conf import settings
+import requests
+
+def sendWhatAppMessage(phoneNumber, message):
+    headers= {"Authourization":settings.WHATSAPP_TOKEN}
+    payload= {"messaging_product":"whatsapp",
+              "recipient_type":"individual",
+              "to":phoneNumber,
+              "type":"text",
+              "text":{"body":message}
+              }
+    response = requests.post(settings.WHATSAPP_URL, headers=headers,json=payload)
+    ans= response.json()
+    
+    
+    
+phoneNumber="0781798350"
+message= "Hello there, \n This is it the first Link up"
+sendWhatAppMessage(phoneNumber, message)
