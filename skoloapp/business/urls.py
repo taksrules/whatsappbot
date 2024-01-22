539fdac528740b1e11b2570e8228bd250dd1108d
taksrules <Takura.mukaro@outlook.com>
2024-01-22 12:59:39 +0200

    WebHook

--- skoloapp/business/urls.py
+++ skoloapp/business/urls.py
@@ -1,4 +1,9 @@
 from django.urls import path
 from . import views
 
-urlpatterns = [path('', views.home, name='home')]
+urlpatterns = [path('', views.home, name='home'),
+               path('c80821c-2d9c-46c5-9049-b4c56b1b469e', views.whatsAppWebhook, name='whatsapp-web'),               
+               ]
+
+#https://taskrules.pythonanywhere.com/c80821c-2d9c-46c5-9049-b4c56b1b469e
+#070ac4a2-76fe-4014-b31b-998e393f0264
--- skoloapp/business/views.py
+++ skoloapp/business/views.py
@@ -1,5 +1,27 @@
+import json
+from django.http import HttpResponse
 from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
+
+from .functions import *
 
 # Create your views here.
 def home(request):
     return render(request, 'business/index.html',{})
+
+@csrf_exempt
+def whatsAppWebhook(request):
+    if request.method== 'GET':
+        VERIFY_TOKEN='070ac4a2-76fe-4014-b31b-998e393f0264'
+        mode= request.GET['hub.mode']
+        token = request.GET['hub.verify_token']
+        challenge= request.GET['hub.challenge']
+        
+        if mode == 'subscribe' and token == VERIFY_TOKEN:
+            return HttpResponse(challenge,status=200)
+        else:
+            return HttpResponse('error', status=403)
+    if request.method=='POST':
+        data= json.loads(request.body)
+        
+        return HttpResponse('success', status=200) 
